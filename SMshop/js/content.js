a0d514e01c9e1ce6c91ee474e3a5f152d96f6be1
lei <739945367@qq.com>
2018-12-01 22:44:53 +0800

    第三次修改

---
+++ SMshop/js/content.js
@@ -0,0 +1,22 @@
+$.ajax({
+	type : "get",
+	url : " json/content.json",
+	async:true,
+	success :function(arr){
+		var str="";
+		for( var i=0; i<arr.length; i++){
+			str+=`<li>
+					<a href="page.html">
+						<img src="img/${arr[i].src}" alt="" />
+						<p>${arr[i].name}</p>
+						<p id="price">${arr[i].price}</p>
+					</a>
+					<div id="ts">
+						<p>劲省200口口新鲜</p>
+						<p>精选大果汁多味甜</p>	
+					</div>
+				</li>`;
+		}
+		$("#ul").html(str);
+	}
+})
--- SMshop/php/register.php
+++ SMshop/php/register.php
@@ -1,22 +1,29 @@
 <?php
 	header("content-type:text/html;charset=utf-8");
-	$db=mysql_connect("locahost","root","root");
-	mysql_select_db("db1819",$db);
+	$db = mysql_connect("localhost","root","root");
+	mysql_select_db( "db1819" , $db );
 	mysql_query("set names utf8");
-	$status =$_GET["status"];
-	$pname =$_GET["pname"];
-	$pemail=$_GET["pemail"];
-	$pwd =$_GET["pwd"];
-	$phone=$_GET["phone"];
+
+
+	$pname=$_GET["uname"];
+	$pemail=$_GET["uemail"];
+	$pwd=$_GET["upwd"];
+	$phone=$_GET["uphone"];                                                                                                                                                                                                                                                                                                                                                                                    
 	
-	if("status"=="register"){
-		//注册功能
-		$sql ="INSERT INTO `shopSM`(`uname`, `uemail`, `upwd`, `phone`) VALUES ('$pname', '$pemail', '$pwd', '$phone' )";
-		$row = mysql_query($sql );
-		if($row){
+	//注册功能
+	//查找shosm中是否存在$pname 如果存在就不注册
+	$sql="select * from shopsm where uname='$pname'";
+	$res=mysql_query($sql);
+	$arr=mysql_fetch_array($res);
+	if($arr){
+		echo 0; //用户名已存在*/
+	}else{
+		$sql2 ="INSERT INTO `shopsm`(`uname`, `uemail`, `upwd`, `phone`) VALUES ('$pname', '$pemail', '$pwd', '$phone' )";
+		$row2 = mysql_query($sql2);
+		if($row2){
 			echo 1;//注册成功
 		}else{
 			echo 2;//注册失败
-		}
+		}			
 	}
 ?>
